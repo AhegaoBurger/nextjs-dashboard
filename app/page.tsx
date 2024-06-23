7c03984f00d266f37144fdf90c814437073083a3
Ahegao_Burger <artur.waysman.2004@mail.ru>
2024-06-23 11:37:18 +0100

    Trying another method to get telegram data.

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,16 @@ import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import { CirclePlus, CircleX, ChevronRight } from 'lucide-react';
 
-import { useTelegram } from '@/lib/TelegramProvider';
+import useTelegramInitData from '@/lib/useTelegramInitData'
 
 export default function Page() {
-  const { telegramUser, webApp } = useTelegram();
-  console.log(telegramUser);
+  const initData = useTelegramInitData();
 
   return (
     <div>
      Hi
-     {telegramUser ? (
-        <div>
-          <h1>Welcome {telegramUser?.username}</h1>
-          User data:
-          <pre>{JSON.stringify(telegramUser, null, 2)}</pre>
-          Eniter Web App data:
-          <pre>{JSON.stringify(webApp, null, 2)}</pre>
-        </div>
-      ) : (
-        <div>Make sure web app is opened from telegram client</div>
-      )}
+     <h1>initData</h1>
+      <pre>{JSON.stringify(initData, null, 2)}</pre>
     </div>
   );
 }
