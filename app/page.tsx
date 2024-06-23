290a0e307ef1d0aa916dad6c085853d7ea5203da
Ahegao_Burger <artur.waysman.2004@mail.ru>
2024-06-23 10:49:23 +0100

    Checking if this thing wokrs.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -6,13 +7,26 @@ import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import { CirclePlus, CircleX, ChevronRight } from 'lucide-react';
 
-export default function Page() {
+import { useTelegram } from '@/lib/TelegramProvider';
 
-  // WebApp.showAlert('Hello world!');
+export default function Page() {
+  const { telegramUser, webApp } = useTelegram();
+  console.log(telegramUser);
 
   return (
     <div>
      Hi
+     {telegramUser ? (
+        <div>
+          <h1>Welcome {telegramUser?.username}</h1>
+          User data:
+          <pre>{JSON.stringify(telegramUser, null, 2)}</pre>
+          Eniter Web App data:
+          <pre>{JSON.stringify(webApp, null, 2)}</pre>
+        </div>
+      ) : (
+        <div>Make sure web app is opened from telegram client</div>
+      )}
     </div>
   );
 }
